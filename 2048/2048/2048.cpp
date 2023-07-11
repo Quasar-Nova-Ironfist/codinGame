416a83c58eab9841870b7312d971c4ec285d4b09
Q <quasarwizard@gmail.com>
2023-07-10 22:45:22 -0500

    a

--- 2048/2048/2048.cpp
+++ 2048/2048/2048.cpp
@@ -15,12 +15,31 @@ struct board {
     */
     long seed;
     int nums[16]{};
-    board() {};
-    board(long seedIn, int p0, int v0, int p1, int v1) {
-        seed = seedIn;
-        nums[p0] = v0;
-        nums[p1] = v1;
+    void spawnTile() {
+        vector<int> freeCells;
+        freeCells.reserve(8);
+        for (int i = 0; i < 16; ++i)
+            if (!nums[i]) {
+                cerr << i << ' ';
+                freeCells.push_back(i);
+            }
+        cerr << endl;
+        cerr << "freeCells.size(): " << freeCells.size() << endl;
+        cerr << "seed: " << seed << endl;
+        int numsIndex = freeCells[(int)seed % freeCells.size()];
+        cerr << "numsIndex: " << numsIndex << endl;
+        int val = (seed & 0x10) ? 4 : 2;
+        cerr << "val: " << val << endl;
+        nums[numsIndex] = val;
+        seed = seed * seed % 50515093L;
+        cerr << "seed: " << seed << endl;
     }
+    board(long seedIn) {
+        seed = seedIn;
+        cerr << "seed: " << seed << endl;
+        spawnTile();
+        spawnTile();
+    };
     board(board& b) {
         seed = b.seed;
         for (int i = 0; i < 16; ++i)
@@ -28,6 +47,7 @@ struct board {
     }
     int collapse(int& n0, int& n1, int& n2, int& n3, bool& anyMoved) {//to the left
         bool merged[3]{};
+
         return 0;
     }
     int makeMove(int dir) {
@@ -60,23 +80,11 @@ struct board {
             break;
         }
         if (anyMoved) {
-            vector<int> freeCells(8);
-            for (int x = 0; x < 4; x++) {
-                for (int y = 0; y < 4; y++) {
-                    if (!nums[x + y * 4]) {
-                        //cerr << x << ", " << y << endl;
-                        freeCells.push_back(x + y * 4);
-                    }
-                }
-            }
-            nums[freeCells[(int)seed % freeCells.size()]]//choose available spot to spawn according to game's algorithm
-                = (seed & 0x10) == 0 ? 2 : 4;//set cell value to 2 or 4, depending
-            seed = seed * seed % 50515093L;
+            spawnTile();
             return moveScoreSum;
         }
         return -9999;
     }
-    long getSeed() { return seed; }//RIF?
     friend std::ostream& operator<<(std::ostream& os, const board& b);
 };
 std::ostream& operator<<(std::ostream& os, const board& b){
@@ -87,47 +95,25 @@ std::ostream& operator<<(std::ostream& os, const board& b){
     } 
     return os;
 }
-void outputMoves(board& current, int time) {
+void outputMovesPlural(board& current, int time) {
     auto startTime = std::chrono::steady_clock::now();//replace with now() + time for checking if past that time
     current.makeMove(0);
     cout << "U" << endl;
 }
-void setup(board& current) {
+int main() {
     long seed;
     cin >> seed; cin.ignore();
-    int score;
-    cin >> score; cin.ignore();
-    vector<int> initCells;
-    initCells.reserve(4);
-    for (int x = 0; x < 4; x++) {
-        for (int y = 0; y < 4; y++) {
-            int cell;
-            cin >> cell; cin.ignore();
-            if (cell) {
-                initCells.push_back(x + y * 4);
-                initCells.push_back(cell);
-            }
-        }
-    }
-    current =  board(seed, initCells[0], initCells[1], initCells[2], initCells[3]);
-}
-int main() {//passed seed probably needs to be advanced twice in setup
-    board current;
-    setup(current);
-    outputMoves(current, 990);
-    while (true) {
-        cerr << current << endl;
-        long seed;
+    board current(seed);
+    for (int i = 0; i < 17; ++i) {
         cin >> seed; cin.ignore();
-        cerr << "Current seed: " << current.getSeed() << endl;;
-        int score;
-        cin >> score; cin.ignore();
-        for (int x = 0; x < 4; x++) {
-            for (int y = 0; y < 4; y++) {
-                int cell;
-                cin >> cell; cin.ignore();
-            }
+    }
+    cerr << current << endl;
+    outputMovesPlural(current, 990);
+    while (true) {
+        for (int i = 0; i < 18; ++i) {
+            cin >> seed; cin.ignore();
         }
-        outputMoves(current, 45);
+        cerr << current << endl;
+        outputMovesPlural(current, 45);
     }
 }
