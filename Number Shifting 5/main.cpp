40fa56f400eceea404ce39e1ddf36b34bdf8e593
Q <quasarwizard@gmail.com>
2023-07-16 23:05:55 -0500

    hyivbl

--- Number Shifting 5/main.cpp
+++ Number Shifting 5/main.cpp
@@ -1,14 +1,10 @@
-#pragma once
 #include "main.h"
+#include "solveState.h"
 #include <iostream>
 #include <fstream>
-#include <set>
-#include <atomic>
-#include <mutex>
 #include <thread>
 
 using std::cout; using std::endl; using std::vector; using std::move; using std::ref;
-const int dirMults[4] = { 0, 1, 0, -1 };// x: i, y: 3 - i
 
 std::set<std::vector<std::vector<int>>> transTable;
 std::atomic_bool stopSearch;
@@ -34,52 +30,15 @@ int main(){
             }
         }
         pwomp.non0s.shrink_to_fit();
-        {
-            for (size_t x = pwomp.cur.size(); x--;) {
-                for (size_t y = pwomp.cur[0].size(); y--;) {
-                    if (pwomp.cur[x][y])
-                        goto pastRightShave;
-                }
-                pwomp.cur.pop_back();
+        trimGrid(pwomp.cur);
+        cout << "\nx, y offset : " << resultOffsetX << ", " << resultOffsetY << '\n';
+        for (size_t y = 0; y < pwomp.cur[0].size(); ++y) {
+            for (size_t x = 0; x < pwomp.cur.size(); ++x) {
+                cout << pwomp.cur[x][y] << ' ';
             }
-        pastRightShave:
-            for (size_t y = pwomp.cur[0].size(); y--;) {
-                for (size_t x = pwomp.cur.size(); x--;) {
-                    if (pwomp.cur[x][y])
-                        goto pastBottomShave;
-                }
-                for (size_t x = pwomp.cur.size(); x--;)
-                    pwomp.cur[x].pop_back();
-            }
-        pastBottomShave:
-            for (size_t y = 0; y < pwomp.cur[0].size(); ++y) {
-                if (pwomp.cur[0][y])
-                    goto pastLeftShave;
-            }
-            pwomp.cur.erase(pwomp.cur.begin());
-            ++resultOffsetX;
-            goto pastBottomShave;
-        pastLeftShave:
-            for (int x = 0; x < pwomp.cur.size(); ++x) {
-                if (pwomp.cur[x][0])
-                    goto pastShaves;
-            }
-            for (int x = 0; x < pwomp.cur.size(); ++x)
-                pwomp.cur[x].erase(pwomp.cur[x].begin());
-            ++resultOffsetY;
-            goto pastBottomShave;
-        pastShaves:
-            pwomp.cur.shrink_to_fit();
-            for (int x = 0; x < pwomp.cur.size(); ++x)
-                pwomp.cur[x].shrink_to_fit();
-            cout << "\nx, y offset : " << resultOffsetX << ", " << resultOffsetY << '\n';
-            for (size_t y = 0; y < pwomp.cur[0].size(); ++y) {
-                for (size_t x = 0; x < pwomp.cur.size(); ++x) {
-                    cout << pwomp.cur[x][y] << ' ';
-                }
-                cout << endl;
-            }
+            cout << endl;
         }
+
         auto solveThreadLambda = [&] {
             solveState a;
             a.cur = pwomp.cur;
@@ -97,79 +56,48 @@ int main(){
     }
 }
 
-bool solveState::solve() {
-    for (int non0sFromIndex = 0; non0sFromIndex < non0s.size(); ++non0sFromIndex) {
-        if (stopSearch)
-            return true;
-        if (checkIfRemovingCardinallyIsolates(cur, non0s[non0sFromIndex].first, non0s[non0sFromIndex].second))
-            continue;
-        int fromX = non0s[non0sFromIndex].first;
-        int fromY = non0s[non0sFromIndex].second;
-        non0s[non0sFromIndex] = non0s.back();
-        non0s.pop_back();
-        int beforeFrom = cur[fromX][fromY];
-        cur[fromX][fromY] = 0;
-
-        for (int dir = 0; dir < 4; ++dir) {
-            if (stopSearch)
-                return true;
-            int toX = fromX + beforeFrom * dirMults[dir];
-            int toY = fromY + beforeFrom * dirMults[3 - dir];
-            if (toX < 0 || toX >= cur.size() || toY < 0 || toY >= cur[0].size() || !cur[toX][toY])
-                continue;
- 
-            int beforeTo = cur[toX][toY];
-            for (int times = -1; times < 2; times += 2) {
-                if (stopSearch)
-                    return true;
-                cur[toX][toY] = abs(beforeTo + beforeFrom * times);
-                if (!cur[toX][toY] && (non0s.size() == 2 || checkIfRemovingCardinallyIsolates(cur, toX, toY)))
-                    continue;
-                transTableMutex.lock();
-                bool contDownBranch = transTable.insert(cur).second;
-                transTableMutex.unlock();
-                if (!contDownBranch)
-                    continue;
-
-                if (!cur[toX][toY]) {//remove matching entry from non0s
-                    for (int i = 0; i < non0s.size(); ++i) {
-                        if (non0s[i].first == toX && non0s[i].second == toY) {
-                            non0s[i] = non0s.back();
-                            non0s.pop_back();
-                            break;
-                        }
-                    }
-                }
-                moves.push_back({fromX, fromY, dir, times});
-                if (!non0s.size()) {//solution found, write to file
-                    stopSearch = true;
-                    std::ofstream outFile("C:/Users/Quasar/source/repos/codinGame/Number Shifting/output.txt", std::fstream::app);
-                    outFile << "cout << \"";
-                    for (int i = 0; i < moves.size(); ++i) {
-                        outFile << moves[i][0] + resultOffsetX << ' ' << moves[i][1] + resultOffsetY << ' ' <<
-                            (moves[i][2] ? (moves[i][2] == 1 ? 'R' : (moves[i][2] == 2 ? 'D' : 'L')) : 'U') << ' ' <<
-                            (moves[i][3] > 0 ? '+' : '-') << "\\n";
-                    }
-                    outFile << "\";" << endl;
-                    outFile.close();
-                    system("taskkill /f /fi \"windowtitle eq output.txt*\"");
-                    system("start notepad \"C:/Users/Quasar/source/repos/codinGame/Number Shifting/output.txt\"");
-                    return true;
-                }
-                if (solve())
-                    return true;
-                moves.pop_back();
-                if(!cur[toX][toY])
-                    non0s.emplace_back(toX, toY);
-            }
-            cur[toX][toY] = beforeTo;
+void trimGrid(std::vector<std::vector<int>>& cur) {
+shaveStart:
+    for (size_t x = cur.size(); x--;) {
+        for (size_t y = cur[0].size(); y--;) {
+            if (cur[x][y])
+                goto pastRightShave;
         }
-        non0s.emplace_back(fromX, fromY);
-        cur[fromX][fromY] = beforeFrom;
+        cur.pop_back();
     }
-    return false;
+    goto shaveStart;
+pastRightShave:
+    for (size_t y = cur[0].size(); y--;) {
+        for (size_t x = cur.size(); x--;) {
+            if (cur[x][y])
+                goto pastBottomShave;
+        }
+        for (size_t x = cur.size(); x--;)
+            cur[x].pop_back();
+    }
+    goto shaveStart;
+pastBottomShave:
+    for (size_t y = 0; y < cur[0].size(); ++y) {
+        if (cur[0][y])
+            goto pastLeftShave;
+    }
+    cur.erase(cur.begin());
+    ++resultOffsetX;
+    goto shaveStart;
+pastLeftShave:
+    for (int x = 0; x < cur.size(); ++x) {
+        if (cur[x][0])
+            goto pastShaves;
+    }
+    for (int x = 0; x < cur.size(); ++x)
+        cur[x].erase(cur[x].begin());
+    ++resultOffsetY;
+    goto shaveStart;
+pastShaves:
+    cur.shrink_to_fit();
+    for (int x = 0; x < cur.size(); ++x)
+        cur[x].shrink_to_fit();
 }
-
 bool checkIfRemovingCardinallyIsolates(std::vector<std::vector<int>>& cur, int x, int y){//make recursive?
     int found = -1;
     for (int checkY = 0; checkY < cur[0].size(); ++checkY) {
@@ -205,4 +133,19 @@ pastUpDown:
         return true;
     }
     return false;
+}
+bool outputToFileAndReturnTrue(std::vector<std::array<int, 4>>& moves) {
+    stopSearch = true;
+    std::ofstream outFile("C:/Users/Quasar/source/repos/codinGame/Number Shifting/output.txt", std::fstream::app);
+    outFile << "cout << \"";
+    for (int i = 0; i < moves.size(); ++i) {
+        outFile << moves[i][0] + resultOffsetX << ' ' << moves[i][1] + resultOffsetY << ' ' <<
+            (moves[i][2] ? (moves[i][2] == 1 ? 'R' : (moves[i][2] == 2 ? 'D' : 'L')) : 'U') << ' ' <<
+            (moves[i][3] > 0 ? '+' : '-') << "\\n";
+    }
+    outFile << "\";" << endl;
+    outFile.close();
+    system("taskkill /f /fi \"windowtitle eq output.txt*\"");
+    system("start notepad \"C:/Users/Quasar/source/repos/codinGame/Number Shifting/output.txt\"");
+    return true;
 }
--- Number Shifting 5/main.h
+++ Number Shifting 5/main.h
@@ -1,10 +1,12 @@
-#pragma once
 #include <vector>
-#include <array>
-struct solveState {
-	std::vector<std::vector<int>> cur;
-	std::vector<std::array<int, 4>> moves;//x, y, dir, times
-	std::vector<std::pair<int, int>> non0s;
-	bool solve();
-};
+#include <set>
+#include <atomic>
+#include <mutex>
 bool checkIfRemovingCardinallyIsolates(std::vector<std::vector<int>>& cur, int x, int y);
+void trimGrid(std::vector<std::vector<int>>& cur);
+bool outputToFileAndReturnTrue(std::vector<std::array<int, 4>>& moves);
+
+extern std::set<std::vector<std::vector<int>>> transTable;
+extern std::atomic_bool stopSearch;
+extern std::mutex transTableMutex;
+extern int resultOffsetX, resultOffsetY;
